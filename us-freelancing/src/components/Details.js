ae2db351df8e648e923d3d45a780a077f7423805
marcusdickens <marcus.r.dickens@gmail.com>
2021-05-13 12:23:28 -0400

    minor changes to details

--- us-freelancing/src/components/Details.js
+++ us-freelancing/src/components/Details.js
@@ -14,14 +14,6 @@ export default class Details extends Component {
                     <div className="card-body">
                         <h5 className="card-title">{name}</h5>
                     </div>
-                    {/* <svg xmlns="http://www.w3.org/2000/svg" className="d-block user-select-none" width="100%" height="200" 
-                        aria-label="Placeholder: Image cap" focusable="false" role="img" preserveAspectRatio="xMidYMid slice" viewBox="0 0 318 180" 
-                        styles="font-size:1.125rem;text-anchor:middle">
-                        <rect width="100%" height="100%" fill="#868e96"></rect>
-                        <text x="50%" y="50%" fill="#dee2e6" dy=".3em"></text>
-                        
-                        
-                    </svg> */}
                     <div >
                         <img src={image} style={{height: 350, width: 350}}/>
                     </div>
